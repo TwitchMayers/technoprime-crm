0a56c41e57732b844641a26b4cfc7657cc7ed143
TwitchMayers <lucky.memory@yandex.ru>
2026-05-27 00:38:19 +0300

    chore: remove confirmed dead legacy code

--- frontend/src/components/ui/GlassCard.tsx
+++
@@ -1,7 +0,0 @@
-'use client';
-import { ReactNode } from 'react';
-import clsx from 'clsx';
-
-export default function GlassCard({ children, className }: { children: ReactNode; className?: string }) {
-  return <div className={clsx('glass p-3', className)}>{children}</div>;
-}
